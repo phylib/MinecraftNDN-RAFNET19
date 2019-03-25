a26b44323494f0420dce5ed5c2d740387d7359da
theuerse <theuers@posteo.de>
2019-03-25 10:06:12 +0100

    Changed data URLs

--- data/clone-data.sh
+++ data/clone-data.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 download () {
-  echo $1
   if [ -f $1 ]; then
      echo "File $1 already exists."
   else
      echo "File $1 does not exist, downloading it."
-     wget -O $1 $2
+     wget -qO- $2 | tar xvz || curl -s $2 | tar xvz
   fi
 }
 
 
 FILE="networkTrace.pcap"
-URL="http://www-itec.aau.at/ftp/icn/minecraft-ndn/RAFNET-19/networkTrace.pcap"
+URL="http://www-itec.aau.at/ftp/icn/minecraft-ndn/RAFNET-19/networkTrace.tar.gz"
 download $FILE $URL
 
-FILE="gamestatechanges.tar.gz"
-URL="http://www-itec.aau.at/ftp/icn/minecraft-ndn/RAFNET-19/gamestatechanges.tar.gz"
+FILE="gameStateChanges.txt"
+URL="http://www-itec.aau.at/ftp/icn/minecraft-ndn/RAFNET-19/gameStateChanges.tar.gz"
 download $FILE $URL
